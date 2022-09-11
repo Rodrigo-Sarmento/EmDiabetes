1a43fec0c1d00fc736595f13c6f080f68000fda9
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-09-11 18:08:48 -0300

    authentication

--- app/src/main/java/com/digo/emdiabetes/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/digo/emdiabetes/ui/auth/LoginFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.digo.emdiabetes.R
 import com.digo.emdiabetes.databinding.FragmentLoginBinding
-
+//import br.com.hellodev.task.helper.BaseFragment
+//import br.com.hellodev.task.helper.FirebaseHelper
+//import br.com.hellodev.task.helper.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,10 +35,14 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
         initClicks()
     }
 
     private fun initClicks() {
+        binding.btnLogin.setOnClickListener { validateData() }
+
         binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
@@ -40,9 +52,48 @@ class LoginFragment : Fragment() {
         }
     }
 
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+
+                //hideKeyboard()
+
+                binding.progressBar.isVisible = true
+
+                loginUser(email, password)
+
+            } else {
+                //showBottomSheet(
+                  //  message = R.string.text_password_empty_login_fragment
+                //)
+            }
+        } else {
+            //showBottomSheet(
+              //  message = R.string.text_email_empty_login_fragment
+            //)
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    //showBottomSheet(
+                      //  message = FirebaseHelper.validError(task.exception?.message ?: "")
+                    //)
+                    binding.progressBar.isVisible = false
+                }
+            }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/digo/emdiabetes/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/digo/emdiabetes/ui/auth/RecoverAccountFragment.kt
@@ -5,20 +5,80 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import com.digo.emdiabetes.R
-
-
-
+import com.digo.emdiabetes.databinding.FragmentRecoverAccountBinding
+//import br.com.hellodev.task.helper.BaseFragment
+//import br.com.hellodev.task.helper.FirebaseHelper
+//import br.com.hellodev.task.helper.initToolbar
+//import br.com.hellodev.task.helper.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recover_account, container, false)
+    ): View {
+        _binding = FragmentRecoverAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //initToolbar(binding.toolbar)
+
+        auth = Firebase.auth
+
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.btnSend.setOnClickListener { validateData() }
+    }
+
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            //hideKeyboard()
+
+            binding.progressBar.isVisible = true
+
+            recoverAccountUser(email)
+        } else {
+            //showBottomSheet(message = R.string.text_email_empty_recover_account_fragment)
+        }
+    }
+
+    private fun recoverAccountUser(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    //showBottomSheet(
+                        //message = R.string.text_email_send_sucess_recover_account_fragment
+                    //)
+                } else {
+                    //showBottomSheet(
+                        //message = FirebaseHelper.validError(task.exception?.message ?: "")
+                    //)
+                }
+
+                binding.progressBar.isVisible = false
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
