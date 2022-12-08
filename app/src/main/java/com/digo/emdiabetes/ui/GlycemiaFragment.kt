7a5b5799e7e26e2f8d2d4ea12cf3c7898606d14d
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-12-08 18:26:17 -0300

    add: alarme de glicemia

--- app/src/main/java/com/digo/emdiabetes/ui/GlycemiaFragment.kt
+++ app/src/main/java/com/digo/emdiabetes/ui/GlycemiaFragment.kt
@@ -1,10 +1,14 @@
 package com.digo.emdiabetes.ui
 
+import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
@@ -14,10 +18,13 @@ import com.digo.emdiabetes.databinding.FragmentGlycemiaBinding
 import com.digo.emdiabetes.helper.FirebaseHelper
 import com.digo.emdiabetes.helper.showBottomSheet
 import com.digo.emdiabetes.model.Glycemia
+import com.digo.emdiabetes.model.Medication
 import com.digo.emdiabetes.ui.adapter.GlycemiaAdapter
+import com.digo.emdiabetes.ui.adapter.MedicationAdapter
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import java.util.*
 
 
 class GlycemiaFragment : Fragment() {
@@ -29,6 +36,11 @@ class GlycemiaFragment : Fragment() {
     private lateinit var glycemiaAdapter: GlycemiaAdapter
 
     private val glycemiaList = mutableListOf<Glycemia>()
+
+    lateinit var timePickerDialog: TimePickerDialog
+    lateinit var calendario: Calendar
+    var horaAtual = 0
+    var minutosAtuais = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -101,6 +113,21 @@ class GlycemiaFragment : Fragment() {
         binding.rvTask.adapter = glycemiaAdapter
     }
 
+    private fun alarme(glycemia: Glycemia){
+        calendario = Calendar.getInstance()
+        horaAtual = calendario.get(Calendar.HOUR_OF_DAY)
+        minutosAtuais = calendario.get(Calendar.MINUTE)
+        timePickerDialog = TimePickerDialog(requireContext(), {
+                timePicker: TimePicker, hourOfDay: Int, minutes: Int ->
+            val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+            intent.putExtra(AlarmClock.EXTRA_HOUR, hourOfDay)
+            intent.putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+            intent.putExtra(AlarmClock.EXTRA_MESSAGE, "Hora de medir a glicemia!")
+            startActivity(intent)
+        },horaAtual,minutosAtuais,true)
+        timePickerDialog.show()
+    }
+
     private fun optionSelect(glycemia: Glycemia, select: Int) {
         when (select) {
             GlycemiaAdapter.SELECT_REMOVE -> {
@@ -110,6 +137,9 @@ class GlycemiaFragment : Fragment() {
                 val action = HomeFragmentDirections
                     .actionHomeFragmentToFormGlycemiaFragment(glycemia)
                 findNavController().navigate(action)
+            }
+            GlycemiaAdapter.SELECT_DETAILS ->{
+                alarme(glycemia)
             }
         }
     }
